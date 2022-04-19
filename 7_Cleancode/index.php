ca6dacf0a31f6d1293d3dd40281a4455fba2977b
Sa-luy <jockersaluy@gmail.com>
2022-04-19 11:43:39 +0700

    magic_value

--- 7_Cleancode/index.php
+++ 7_Cleancode/index.php
@@ -3,10 +3,10 @@
 include_once "Calculator.php";
 
 $calculator = new Calculator();
-echo $calculator->calculate(1,2, '-');
+echo $calculator->calculate(1,2, SUBTRACTION);
 echo "<br>";
-echo $calculator->calculate(1,2, '+');
+echo $calculator->calculate(1,2, ADDITION);
 echo "<br>";
-echo $calculator->calculate(1,2, '*');
+echo $calculator->calculate(1,2, MUTIPLICATION);
 echo "<br>";
-echo $calculator->calculate(1,0, '/');
+echo $calculator->calculate(1,0, DIVISIION);
