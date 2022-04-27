de2023369cf0d91ae437df1e9333e215fc696785
Sa-luy <jockersaluy@gmail.com>
2022-04-27 15:05:07 +0700

    insertSort

---
+++ 12_Sort/insertSort.php
@@ -0,0 +1,24 @@
+<?php
+function swap(&$x, &$y)
+{
+    $tmp = $x;
+    $x = $y;
+    $y = $tmp;
+}
+function insertSort($arr)
+{
+    for ($i = 1; $i < count($arr); $i++) {
+        $value = $arr[$i];
+        $j = $i;
+        while ($j > 0 && $value < $arr[$j - 1]) {
+            $arr[$j] = $arr[$j - 1];
+            $j--;
+        }
+        $arr[$j] = $value;
+    }
+    return $arr;
+}
+$arr = [5, -4, 3, 7, 2, 1, 0, 8, 9, 2];
+$newArr = insertSort($arr);
+echo "<pre>";
+print_r($newArr);
