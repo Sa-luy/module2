74d075b818b71e2946317b19bb662580c61f8573
Sa-luy <jockersaluy@gmail.com>
2022-04-29 14:01:00 +0700

    Account

--- 14_ Regex/checkin.php
+++ 14_ Regex/checkin.php
@@ -13,10 +13,12 @@
 
 // $str = "Em chua18 abc 29 abc 20";
 // print_r(preg_replace("/[0-9]+/", "Saluy", $str));
-$subject = "CodeGym@gmail.com";
-$pattern = '/^\w+@\w+(\.\w+){1,3}$/';
-if (preg_match($pattern, $subject, $matches)) {
-    echo 'Khớp';
-} else {
-    echo 'Không khớp';
-}
+// $subject = "CodeGym@gmail.com";
+// $pattern = '/^\w+@\w+(\.\w+){1,3}$/';
+// if (preg_match($pattern, $subject, $matches)) {
+//     echo 'Khớp';
+// } else {
+//     echo 'Không khớp';
+// }
+var_dump(10) . '<br>';
+var_dump(acos(10));
