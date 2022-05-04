e7200404651b139c9b272952271c7c71b7b1e0e3
Sa-luy <jockersaluy@gmail.com>
2022-05-04 20:41:44 +0700

    0000

--- text.php
+++ text.php
@@ -1,4 +1,13 @@
 <?php
-const A = 3;
-A+=3
-echo A;
+function sort_ascending($arr, $element)
+{
+    for ($i = count($arr) - 1; $i >= 0; $i--) {
+        array_push($element, $arr[$i]);
+    }
+    $arr = $element;
+    return $arr;
+}
+$element = [];
+$arr = [1, 4, 2, 56, 3];
+echo '<pre>';
+print_r(sort_ascending($arr, $element));
